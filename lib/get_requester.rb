6a1d3fc8d9b266299a0edd489ed82fe9416f2719
cassymarie <cassy2205@gmail.com>
2020-11-06 12:43:26 -0700

    commit tree 0862cfe0c5ff1ee4cffc60b2784213741259d0d3

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,29 @@
 # Write your code here
+require 'net/http'  
+require 'json'      #gem install json (if get error)
+require 'open-uri'
+
+# url = "https://cat-fact.herokuapp.com/facts"
+# url = URL(url)
+
+# response = Net::HTTP.get(url)
+
+# apis = JSON.parse(response)
+
+class GetRequester
+     attr_accessor :link
+
+     def initialize(link)
+          @link =  link
+     end
+
+     def get_response_body
+          uri = URI.parse(link)
+          response = Net::HTTP.get_response(uri)
+          response.body
+     end
+
+     def parse_json
+          JSON.parse(get_response_body)
+     end
+end
